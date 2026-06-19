0ad8739359e962ee611b545018dcb54dc871fecc
Gene <64043816+Gene-avatech@users.noreply.github.com>
2026-06-18 20:09:10 -0700

    feat(ui): HoverCursorBadge action icon by cursor

--- src/components/WorldCanvas.vue
+++ src/components/WorldCanvas.vue
@@ -1,12 +1,14 @@
 <script setup>
 import { ref } from 'vue'
 import { useWorldEngine } from '@/composables/useWorldEngine'
+import HoverCursorBadge from '@/components/HoverCursorBadge.vue'
 const canvasRef = ref(null)
-useWorldEngine(canvasRef)
+const { hoverAction, hoverPos } = useWorldEngine(canvasRef)
 </script>
 
 <template>
   <div class="relative w-full h-full">
     <canvas ref="canvasRef" class="w-full h-full block" />
+    <HoverCursorBadge :action="hoverAction" :x="hoverPos.x" :y="hoverPos.y" />
   </div>
 </template>
